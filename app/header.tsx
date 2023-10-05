0774796b1b3de9494e8628d70851961f2f253947
is-uko <dfdba805@gmail.com>
2023-10-05 16:06:03 +0900

    ヘッダーを修正

--- app/header.tsx
+++ app/header.tsx
@@ -12,8 +12,8 @@ export function Header() {
       <div
         className={
           isOpen
-            ? "fixed w-screen h-screen bg-red-600 duration-500"
-            : "fixed w-screen h-screen bg-red-600 bg-opacity-0 pointer-events-none duration-500"
+            ? "fixed w-screen h-screen bg-red-600 duration-500 z-20"
+            : "fixed w-screen h-screen bg-red-600 bg-opacity-0 pointer-events-none duration-500 z-20"
         }
       >
         <nav
@@ -28,12 +28,14 @@ export function Header() {
             <li className="pb-5">受講生の声</li>
             <li>スクールの概要</li>
           </ul>
-          <button className="relative px-12 py-3 border-2 border-black after:absolute after:-bottom-2 after:-right-2 after:w-60 after:h-12 after:border-b-2 after:border-r-2 after:border-black hover:bg-black hover:text-red-600 duration-500">無料体験に申し込む</button>
+          <button className="relative px-12 py-3 border-2 border-black after:absolute after:-bottom-2 after:-right-2 after:w-60 after:h-12 after:border-b-2 after:border-r-2 after:border-black hover:bg-black hover:text-red-600 duration-500">
+            無料体験に申し込む
+          </button>
         </nav>
       </div>
       <div className="flex justify-between">
         <Image
-          className="h-14 mt-5 mb-5 z-10"
+          className="h-14 mt-5 mb-5 z-20"
           src={isOpen ? "img/logo-w.svg" : "img/logo-r.svg"}
           alt=""
           width={150}
@@ -42,30 +44,30 @@ export function Header() {
         <button
           className={
             isOpen
-              ? "relative h-24 w-24 bg-red-600 duration-500"
-              : "relative h-24 w-24 bg-red-600 hover:bg-red-800 duration-500"
+              ? "relative h-24 w-24 bg-red-600 duration-500 z-20"
+              : "relative h-24 w-24 bg-red-600 hover:bg-red-800 duration-500 z-20"
           }
           onClick={handleClick}
         >
           <span
             className={
               isOpen
-                ? "absolute top-8 left-6 h-[2px] w-12 bg-black translate-y-3.5 rotate-45 duration-500"
-                : "absolute top-8 left-6 h-[2px] w-12 bg-black z-10 duration-500"
+                ? "absolute top-8 left-6 h-[2px] w-12 bg-black translate-y-3.5 rotate-45 duration-500 z-20"
+                : "absolute top-8 left-6 h-[2px] w-12 bg-black z-20 duration-500"
             }
           ></span>
           <span
             className={
               isOpen
-                ? "top-8 left-6 h-[2px] w-12 bg-red-600 translate-y-3.5 rotate-45 duration-500"
-                : "absolute top-12 left-6 h-[2px] w-12 bg-black z-10 duration-500"
+                ? "top-8 left-6 h-[2px] w-12 bg-red-600 translate-y-3.5 rotate-45 duration-500 z-20"
+                : "absolute top-12 left-6 h-[2px] w-12 bg-black z-20 duration-500"
             }
           ></span>
           <span
             className={
               isOpen
-                ? "absolute top-8 left-6 h-[2px] w-12 bg-black translate-y-3.5 -rotate-45 duration-500"
-                : "absolute top-16 left-6 h-[2px] w-12 bg-black z-10 duration-500"
+                ? "absolute top-8 left-6 h-[2px] w-12 bg-black translate-y-3.5 -rotate-45 duration-500 z-20"
+                : "absolute top-16 left-6 h-[2px] w-12 bg-black z-20 duration-500"
             }
           ></span>
         </button>
